7673b5f354f9538271597f71d20a8110c664ae00
tommy.li <tommy.li@firefire.co>
2023-02-20 20:56:48 +1100

    feat: Gone further with calling asset.get
    * Need help with new error message about No permission to access metadata

--- src/main/groovy/mcri/mf/Main.groovy
+++ src/main/groovy/mcri/mf/Main.groovy
@@ -2,5 +2,7 @@ package mcri.mf
 
 static void main(String[] args) {
     MfClient underTest = MfClient.instance
+    println "services: ${underTest.systemServiceList().join('\n')}"
+    underTest.actorSelfDescribe()
     underTest.showAsset(747719513)
 }
--- src/main/groovy/mcri/mf/MfClient.groovy
+++ src/main/groovy/mcri/mf/MfClient.groovy
@@ -35,14 +35,24 @@ class MfClient {
     private String authConn(RemoteServer server) {
         MfConnection authConn = (MfConnection) server.open()
         authConn.connect(MF_APP, mfDomain, mfUsername, mfPassword)
+
         Writer writer = new StringWriter()
         MarkupBuilder xml = new MarkupBuilder(writer)
+
+        //<app>archie</app>
+        //<destroy-on-service-call>server.version</destroy-on-service-call>
+        //<perm>
+        //  <access>ACCESS</access>
+        //  <resource type='service'>asset.get</resource>
+        //</perm>
         xml.app(MF_APP)
         xml.'destroy-on-service-call'('server.version')
         xml.perm {
             access('ACCESS')
-            resource(type: 'service', 'server.version')
+            resource(type: 'service', 'asset.get')
         }
+        // HELP: Is there a way to request for multiple resources when creating a token?
+
         def r = authConn.execute("secure.identity.token.create", writer.toString())
         String token = r.value("token")
         return token
@@ -50,16 +60,29 @@ class MfClient {
 
     Collection<String> systemServiceList() {
         def r = conn.execute("system.service.list")
+//        def r = conn.execute("system.service.describe")
         Collection<String> services = r.values("service");
         return services
     }
 
-    void showAsset(Long id) {
+    void actorSelfDescribe() {
+        def r = conn.execute("actor.self.describe")
+        println r
+    }
+
+    void showAsset(Long assetId) {
         Writer writer = new StringWriter()
         MarkupBuilder xml = new MarkupBuilder(writer)
-        xml.app(MF_APP)
-        xml.id(id)
-        def ele = conn.execute('asset.get', writer.toString())
+
+//         <service name='asset.get'>
+//           <id>747719513</id>
+//         </service>
+        xml.service(name: 'asset.get') {
+            id(assetId)
+        }
+
+        // HELP: asset.get: call to service 'asset.get' failed: No permission to access metadata for asset (id) 97485468
+        def ele = conn.execute('service.execute', writer.toString())
         println ele
     }
 }
